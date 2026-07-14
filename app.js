d5133b3496eb83b621a93371d1812fd59a9c2c22
ycsal <yascsalgado@gmail.com>
2026-07-14 00:17:16 -0300

    finalizando o projeto

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
+let listaDeChutes = [];
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate: 1.2});
 }
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Digite um número entre 1 e 100 e tente adivinhar o número secreto!');
+    exibirTextoNaTela('p', 'Digite um número entre 1 e 10 e tente adivinhar o número secreto!');
 }
 
 exibirMensagemInicial();
@@ -16,6 +20,16 @@ console.log(numeroSecreto);
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
+    
+    //Verifica se o chute já foi inserido antes
+    if (listaDeChutes.includes(chute)){
+        exibirTextoNaTela('p.observacao', `Você já inseriu o número ${chute} antes. Tente outro número!`);
+        limparInput();
+        return;
+    }
+    
+    exibirTextoNaTela('p.observacao', '');
+    listaDeChutes.push(chute);
     if (chute == numeroSecreto){
         exibirTextoNaTela('h1', 'Parabéns!');
         let palavraTentativas = tentativas === 1 ? 'tentativa' : 'tentativas';
@@ -36,7 +50,20 @@ function verificarChute(){
 }
 
 function gerarNumeroAleatorio(){
-    return parseInt(Math.random() * 100 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if(quantidadeDeElementosNaLista === numeroLimite){
+        listaDeNumerosSorteados = [];
+    }
+
+    if( listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
 }
 
 function limparInput(){
@@ -47,6 +74,7 @@ function limparInput(){
 function reiniciarJogo(){
     numeroSecreto = gerarNumeroAleatorio();
     tentativas = 1;
+    listaDeChutes = [];
     exibirMensagemInicial();
     console.log(numeroSecreto);
     document.getElementById('reiniciar').setAttribute('disabled', true);
